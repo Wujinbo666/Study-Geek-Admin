601088e10dafb123a98cfa41281f0bbf84ce3e7d
bigfacebobo <1136046731@qq.com>
2022-08-25 17:01:15 +0800

    临时提交

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+// element plus
+import ElementPlus from "element-plus";
+// element icons
+import * as Icons from "@element-plus/icons-vue";
+import "element-plus/dist/index.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// 注册element Icons组件
+Object.keys(Icons).forEach((key) => {
+  app.component(key, Icons[key as keyof typeof Icons]);
+});
+
+app.use(ElementPlus).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(), // 增加下面的配置项,这样在运行时就能检查eslint规范
+    eslintPlugin({
+      include: ["src/**/*.js", "src/**/*.vue", "src/*.js", "src/*.vue"],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    // 服务器主机名，如果允许外部访问，可设置为 "0.0.0.0"
+    host: "0.0.0.0",
+    port: 8080,
+    cors: true,
+    // 代理跨域（mock 不需要配置跨域，直接能访问，这里只是个示例）
+    proxy: {
+      "/api": {
+        // target: "https://www.fastmock.site/mock/f81e8333c1a9276214bcdbc170d9e0a0", // fastmock
+        target: "https://mock.mengxuegu.com/mock/629d727e6163854a32e8307e", // easymock
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
